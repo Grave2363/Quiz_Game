7d42d22afdb98859fe4480fd664de82f1f6946c1
Angelo Nicotra <AGNicotra@student.fullsail.edu>
2021-11-15 10:53:23 -0500

    new update

--- lib/Quizes/Quiz01/Quiz_01_Q2.dart
+++ lib/Quizes/Quiz01/Quiz_01_Q2.dart
@@ -1,19 +1,23 @@
 import 'package:flutter/material.dart';
-
-import 'Quiz_01_Q2.dart';
-class Quiz_01_Q1 extends StatefulWidget {
-  const Quiz_01_Q1({Key? key}) : super(key: key);
+class Quiz_01_Q2 extends StatefulWidget {
+  final score;
+  final Q2;
+  final A1;
+  final A2;
+  final A3;
+  final A4;
+  const Quiz_01_Q2({Key? key, this.score, this.A1, this.A2, this.A3, this.A4, this.Q2}) : super(key: key);
 
   @override
-  _Quiz_01_Q1State createState() => _Quiz_01_Q1State();
+  _Quiz_01_Q2State createState() => _Quiz_01_Q2State();
 }
 
-class _Quiz_01_Q1State extends State<Quiz_01_Q1> {
-  String Q2 = "temp question";
-  String A1_Q2 = "temp Ans";
-  String A2_Q2 = "temp Ans";
-  String A3_Q2 = "temp Ans";
-  String A4_Q2 = "temp Ans";
+class _Quiz_01_Q2State extends State<Quiz_01_Q2> {
+  String Q3 = "temp question";
+  String A1_Q3 = "temp Ans";
+  String A2_Q3 = "temp Ans";
+  String A3_Q3 = "temp Ans";
+  String A4_Q3 = "temp Ans";
 
   @override
   Widget build(BuildContext context) {
@@ -28,15 +32,15 @@ class _Quiz_01_Q1State extends State<Quiz_01_Q1> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                Text("Lets start simple, whats 2+2?", textScaleFactor: 2,),
+                Text(widget.Q2, textScaleFactor: 2,),
                 TextButton(
                   style: ButtonStyle(
                     backgroundColor: MaterialStateProperty.all<Color>(Colors.red),
                   ),
-                  child: Text('                       4                       ', style: TextStyle(color: Colors.black, fontSize: 20)),
+                  child: Text(widget.A1, style: TextStyle(color: Colors.black, fontSize: 20)),
                   onPressed: () async {
                     await Navigator.of(context).push(MaterialPageRoute(
-                        builder: (context) => Quiz_01_Q2(score: 1,A1: A1_Q2,A2: A2_Q2,A3: A3_Q2,A4: A4_Q2,Q2: Q2)
+                        builder: (context) => Quiz_01_Q2()
                     ),
                     );
                   },
@@ -45,10 +49,10 @@ class _Quiz_01_Q1State extends State<Quiz_01_Q1> {
                   style: ButtonStyle(
                     backgroundColor: MaterialStateProperty.all<Color>(Colors.red),
                   ),
-                  child: Text('                       5                       ', style: TextStyle(color: Colors.black, fontSize: 20)),
+                  child: Text(widget.A2, style: TextStyle(color: Colors.black, fontSize: 20)),
                   onPressed: () async {
                     await Navigator.of(context).push(MaterialPageRoute(
-                        builder: (context) => Quiz_01_Q2(score: 0,A1: A1_Q2,A2: A2_Q2,A3: A3_Q2,A4: A4_Q2,Q2: Q2)
+                        builder: (context) => Quiz_01_Q2()
                     ),
                     );
                   },
@@ -57,10 +61,10 @@ class _Quiz_01_Q1State extends State<Quiz_01_Q1> {
                   style: ButtonStyle(
                     backgroundColor: MaterialStateProperty.all<Color>(Colors.red),
                   ),
-                  child: Text('    go back to first answer    ', style: TextStyle(color: Colors.black, fontSize: 20)),
+                  child: Text(widget.A3, style: TextStyle(color: Colors.black, fontSize: 20)),
                   onPressed: () async {
                     await Navigator.of(context).push(MaterialPageRoute(
-                        builder: (context) => Quiz_01_Q2(score: 0,A1: A1_Q2,A2: A2_Q2,A3: A3_Q2,A4: A4_Q2,Q2: Q2)
+                        builder: (context) => Quiz_01_Q2()
                     ),
                     );
                   },
@@ -69,10 +73,10 @@ class _Quiz_01_Q1State extends State<Quiz_01_Q1> {
                   style: ButtonStyle(
                     backgroundColor: MaterialStateProperty.all<Color>(Colors.red),
                   ),
-                  child: Text('You went too far, return to 1', style: TextStyle(color: Colors.black, fontSize: 20)),
+                  child: Text(widget.A4, style: TextStyle(color: Colors.black, fontSize: 20)),
                   onPressed: () async {
                     await Navigator.of(context).push(MaterialPageRoute(
-                        builder: (context) => Quiz_01_Q2(score: 0,A1: A1_Q2,A2: A2_Q2,A3: A3_Q2,A4: A4_Q2,Q2: Q2)
+                        builder: (context) => Quiz_01_Q2()
                     ),
                     );
                   },
